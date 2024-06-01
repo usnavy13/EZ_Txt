b755488c357bfcc507398a78a64a7d5d705b42b0
Joe Licata <joelicata.us@gmail.com>
2024-06-01 18:27:34 -0400

    chore: Update main.py with text extraction functionality and dependencies

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # EZ_Txt
-extract text from doc using unstructured and 
+
--- main.py
+++ main.py
@@ -4,17 +4,32 @@ import os
 from unstructured_client import UnstructuredClient
 from unstructured_client.models import shared
 from unstructured_client.models.errors import SDKError
+from langchain_community.document_loaders import AzureAIDocumentIntelligenceLoader
 import gradio as gr
 
 load_dotenv()
 
+azure_doc_intel_endpoint = os.getenv("azure_doc_intel_endpoint")
+azure_doc_intel_key = os.getenv("azure_doc_intel_key")
+unstruct_api_key = os.getenv("unstruct_api_key")
+
 client = UnstructuredClient(
-    api_key_auth=os.getenv("unstruct_api_key"),
+    api_key_auth=unstruct_api_key,)
     # you may need to provide your unique API URL
     # server_url="YOUR_API_URL",
-)
 
-def text_extract(filename):
+
+def azure_text_extract(file_path):
+    loader = AzureAIDocumentIntelligenceLoader(
+        api_endpoint=azure_doc_intel_endpoint, api_key=azure_doc_intel_key, file_path=file_path, api_model="prebuilt-layout")
+
+    documents = loader.load()
+    text = documents[0].page_content
+    return text
+
+
+
+def unstruct_text_extract(filename):
     file = open(filename, "rb")
     req = shared.PartitionParameters(
         # Note that this currently only supports a single file
@@ -33,6 +48,28 @@ def text_extract(filename):
     except SDKError as e:
         return f"Error: {e}"
     
+
+def text_extract_manager(file):
+    # Check if azure_doc_intel_key and azure_doc_intel_endpoint are non-empty strings
+    AZURE_SETUP = bool(azure_doc_intel_key) and bool(azure_doc_intel_endpoint)
+    UNSTRUCT_SETUP = bool(unstruct_api_key)
+    print(AZURE_SETUP, UNSTRUCT_SETUP)
+    if AZURE_SETUP and UNSTRUCT_SETUP:
+        selected = os.getenv("service")
+        print(selected)
+        if selected == "azure":
+
+            return azure_text_extract(file)
+        elif selected == "unstruct":
+  
+            return unstruct_text_extract(file)
+    elif AZURE_SETUP:
+
+        return azure_text_extract(file)
+    elif UNSTRUCT_SETUP:
+
+        return unstruct_text_extract(file)
+    
 with gr.Blocks(title="EZ Text Extractor") as demo:
     gr.Markdown("# Extract text from a document!")
     with gr.Row():
@@ -43,7 +80,7 @@ with gr.Blocks(title="EZ Text Extractor") as demo:
         text = gr.Textbox(label="Extracted Text!",
                           show_copy_button=True)
     
-    run.click(text_extract, file, outputs=text)
+    run.click(text_extract_manager, file, outputs=text)
 
 
 username = os.getenv("user")
